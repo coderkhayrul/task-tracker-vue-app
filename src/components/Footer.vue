ba655cb8b351a38003c7c3dbf893ab8c3b0fa301
Khayrul Islam Shanto <77426516+coderkhayrul@users.noreply.github.com>
2023-02-04 02:59:12 +0600

    Route System Add

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+    <footer>
+        <p>Copyright &copy; 2023</p>
+        <router-link to="/about"> About</router-link>
+    </footer>
+</template>
+
+
+<style scoped>
+    footer {
+        margin-top: 30px;
+        text-align: center;
+    }
+</style>
